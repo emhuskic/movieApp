fb3f170ec8e5040104284ff7afffd8711ac15c5d
Emina huskic <huskic.emina@gmail.com>
2016-02-02 10:55:28 +0100

    Initial Commit

---
+++ movieApp/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  movieApp
+//
+//  Created by Adis Cehajic on 02/02/16.
+//  Copyright © 2016 EminaHuskic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
