b9ef56236ea34096883d6b6efe4fd4a59d2fd52b
Emina huskic <huskic.emina@gmail.com>
2016-02-09 12:37:07 +0100

    with xcodeproj

---
+++ MOVMovieCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MOVMovieCollectionViewCell.h
+//  
+//
+//  Created by Adis Cehajic on 05/02/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MOVMovieCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ MOVMovieCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  MOVMovieCollectionViewCell.m
+//  
+//
+//  Created by Adis Cehajic on 05/02/16.
+//
+//
+
+#import "MOVMovieCollectionViewCell.h"
+
+@implementation MOVMovieCollectionViewCell
+
+@end
